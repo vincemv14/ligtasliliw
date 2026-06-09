77a5282fcb316694ac126f6c493d7c3ba97ba49d
Vince Villanueva <vincemitravillanueva@gmail.com>
2026-06-09 10:15:08 +0800

    chore: explicitly set base path and build outDir

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures asset paths map perfectly to relative locations
+  build: {
+    outDir: 'dist', 
+  },
 })
